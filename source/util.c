36d07f15d8a8b2745b53de04e90327eeca17bc46
gatsby <i-careforu@foxmail.com>
2015-10-18 17:30:18 +0800

    添加新文件

---
+++ source/util.c
@@ -0,0 +1,15 @@
+/*
+	@Author: gatsby
+	@Mail: i-careforu@foxmail.com
+	@Comment: 实用的工具函数集合。
+*/
+
+#include <base.h>
+
+// 解析环境变量
+void *env(char **pairs)
+{
+	char *cp = *pairs;
+	return NULL;
+}
+
