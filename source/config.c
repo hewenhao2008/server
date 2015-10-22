a0476d75f732a749316c99f0ea1fee4613937a5d
gatsby <i-careforu@foxmail.com>
2015-10-22 22:52:19 +0800

    增加配置文件模块

---
+++ source/config.c
@@ -0,0 +1,25 @@
+/*
+    @Author: gatsby
+    @Data: 2015-10-22
+    @Mail: i-careforu@foxmail.com
+    @Comment: 解析配置文件。
+*/
+
+#include <config.h>
+
+/* 映射宏＠将文本映射至ｃ语言结构体 */
+#define BEGIN_MAP(list)
+
+/* 映射宏＠将文本映射至c语言结构体 */
+#define END_MAP
+
+/* config句柄 */
+typedef struct __config
+{
+｝config;
+
+/* 加载配置文件 */
+HCONFIG STDCALL loadconf(const char *path)
+{
+    return 0;
+}
---
+++ source/config.h
@@ -0,0 +1,22 @@
+/*
+    @Author: gatsby
+    @Data: 2015-10-22
+    @Mail: i-careforu@foxmail.com
+    @Comment: 解析配置文件。 
+*/
+
+#ifndef __CONFIG_H_
+#define __CONFIG_H_
+
+#include <base.h>
+
+/* config句柄 */
+typedef void *HCONFIG;
+
+/* 获取conf文件句柄 */
+HCONFIG STDCALL loadconf(const char *path);
+
+/* 获取conf文件一个选项的值 */
+
+
+#endif
