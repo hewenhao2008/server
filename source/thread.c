d24cf2d72e18a17f8e923d38111a59b11d899d16
gatsby <i-careforu@foxmail.com>
2015-11-04 18:14:32 +0800

    code review

--- source/thread.c
+++ source/thread.c
@@ -8,8 +8,11 @@
 #include <base.h>
 #include <thread.h>
 
+/* @Note: 原子操作部分借鉴linux内核中原子操作的实现 */
+
 /* x86架构的lock指定前缀 */
-/* @Note: 单核CPU中一条机器指令始终为原子操作，无需使用lock指定前缀，多核CPU中多核同时访问一个内存单元，可能会造成问题，所以需lock指令前缀锁定cpu地址总线(ABUS) */
+/* @Note: 单核CPU中一条机器指令始终为原子操作，无需使用lock指定前缀，多核CPU中存在多核并行访问同一个内存单元的可能，所以需lock指令前缀锁定cpu地址总线(ABUS) */
+/* @Note: 移除linux内核中smp内存补丁的机制 */
 #ifndef UP
 #define LOCK_PREFIX "lock"
 #else
@@ -53,6 +56,43 @@ static inline void atomic_sub(int i, atomic_t *v)
 		:"+m"(v->counter)
 		:"ir"(i)
 	);
+}
+
+/* 原子操作 - 自增一 */
+static inline void atomic_inc(atomic_t *v)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "incl %0"
+		:"+m"(v->counter)
+	);
+}
+
+/* 原子操作 - 自减一 */
+static inline void atomic_dec(atomic_t *v)
+{
+	__asm__ __volatile__(
+		LOCK_PREFIX "decl %0"
+		:"+m"(v->counter)
+	);
+}
+
+/* 原子操作　- 自增之后并返回值 */
+static inline int atomic_add_return(int i, atomic_t *v)
+{
+	int _i;
+	/* 386 CPU */
+	#ifdef _386
+	#error("too old arch.")
+	#endif
+	/* 486之后的数据 */
+	_i = i;
+	__asm__ __volatile__(
+		LOCK_PREFIX "xaddl %0, %1"
+		:"+r"(_i), "+m"(v->counter)
+		::"memory"
+	);
+	
+	return _i + i;
 }
 
 /* 开启新线程 */
