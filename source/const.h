d4c2243537147308d66be52e956b56180168f44b
wanglanwei <wanglanwei@pe.vc>
2016-01-27 17:19:13 +0800

    加入本地套接字

--- source/const.h
+++ source/const.h
@@ -4,6 +4,8 @@
 #define SERVAPP "SERV"
 #define VERSION "1.0.1"
 
+#define UNIX_SOCKET "/var/run/serv101.socket"
+
 #define version() do{\
 	fprintf(stdout, "%s %s\n", SERVAPP, VERSION); exit(0);\
 }while(0)
