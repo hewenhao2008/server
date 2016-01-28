d93177ddaff9f93753b9e2f3b06ee9ad6cf2749a
王兰卫 <wanglanwei@wanglanweideMacBook-Pro.local>
2016-01-28 10:40:13 +0800

    增加字典实现

--- source/dict.h
+++ source/dict.h
@@ -2,14 +2,18 @@
     @Author: gatsby
     @Data: 2016-01-27
     @Mail: i-careforu@foxmail.com
-    @Note: 哈希表的C语言实现。
+    @Note: 哈希表的C语言实现（借鉴了redis的hash类型实现）。
 */
 #ifndef __DICT_H_
 #define __DICT_H_
 
 #include <stdint.h>
 
+/* Unused arguemts generate annoying warings...*/
 #define DICT_NOTUSED(V) ((void) V)
+
+#define DICT_OK		0
+#define DICT_ERR	1
 
 typedef struct dictEntry
 {
@@ -19,6 +23,7 @@ typedef struct dictEntry
 		void *val;
 		int64_t s64;
 		uint64_t u64;
+		double d;
 	}v;
 	struct dictEntry *next;
 }dictEntry;
