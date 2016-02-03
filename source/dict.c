66bdfc289217fdccbd7e2d4b9c5c219b99f85af7
wanglanwei <wanglanwei@pe.vc>
2016-02-03 15:49:29 +0800

    添加字典的实现‘ “

--- source/dict.c
+++ source/dict.c
@@ -1,1 +1,151 @@
 #include <dict.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#include <sys/time.h>
+
+static int dict_can_resize = 1;
+
+/******************api implemention**************************/
+/*Reset a hash table already initialized with ht_init().
+ *Note: this function should only be called by ht_destroy(). */
+static void _dictReset(dictht *ht)
+{
+    ht->table = NULL;
+    ht->size = 0;
+    ht->sizemask = 0;
+    ht->used = 0;
+}
+
+/* Initialize the hash table. */
+/* 初始化哈希表 */
+static int _dictInit(dict *d, dictType *type, void *privdata)
+{
+    _dictReset(&d->ht[0]);
+    _dictReset(&d->ht[1]);
+    d->type = type;
+    d->privdata = privdata;
+    d->rehashidx = -1;
+    d->iterators = 0;
+    return DICT_OK;
+}
+
+/* create a new hashtable. */
+dict *dictCreate(dictType *type, void *privdata) 
+{
+    dict *d = (dict *)malloc(sizeof(*d));
+    return (d == NULL ? NULL : d);
+}
+
+/*调整哈希表的大小*/
+int dictResize(dict *d)
+{
+    int minimal;
+
+    if(!dict_can_resize || dictIsRehashing(d)) return DICT_ERR;
+    minimal = d->ht[0].used;
+    if(minimal < DICT_HT_INITIAL_SIZE)
+        minimal = DICT_HT_INITIAL_SIZE;
+    return dictExpand(d, minimal);
+}
+
+/* Our hash table capability is a power of two */
+/* 哈希表的容量是以2的指数增长的 */
+static unsigned long _dictNextPower(unsigned long size)
+{
+    unsigned long i = DICT_HT_INITIAL_SIZE;
+
+    if(size >= LONG_MAX) return LONG_MAX;
+    for(;;) {
+        if(i >= size)
+            return i;
+        i *= 2;
+    }
+    return 0;
+}
+
+/* Expand or create the hash table. */
+int dictExpand(dict *d, unsigned long size)
+{
+    dictht n; // the new hashtable.
+    unsigned long realsize = _dictNextPower(size);
+
+    /* The size is invalid if it is smaller then the number 
+     * of elements already inside the hash table*/
+    if(dictIsRehashing(d) || d->ht[0].used > size) return DICT_ERR;
+
+    /* Rehashing to the same table size is not useful. */
+    if (realsize  == d->ht[0].size) return DICT_ERR;
+
+    /* Allocate the new hash table and initialized all pointers to NULL. */
+    n.size = realsize;
+    n.sizemask = realsize - 1;
+    n.table = (dictEntry **)malloc(realsize * sizeof(dictEntry *)); // 指向指针的指针
+
+    /* Is this the first initialization? If so it't not really a rehashing
+     * we just set the first hash table so that it can accept keys.*/
+    if(d->ht[0].table == NULL) {
+        d->ht[0] = n;
+        return DICT_OK;
+    }
+
+    /* Prepare a second hash table for incremental rehashing. */
+    d->ht[1] = n;
+    d->rehashidx = 0;
+    return DICT_OK;
+}
+
+/* 渐进式，进行rehash操作*/
+int dictRehash(dict *d, int n) {
+    // 遍历空桶最大数目
+    int empty_visits = n*10; /* Max number of empty bucket to visit.*/
+
+    if(!dictIsRehashing(d)) return 0;
+    while(n-- && d->ht[0].used != 0) {
+        dictEntry *de, *nextde;
+        while(d->ht[0].table[d->rehashidx] == NULL) {
+            d->rehashidx++;
+            if(--empty_visits == 0)  return DICT_ERR;
+        }
+        // 将旧的哈希联表移动至新的哈希链表
+        de = d->ht[0].table[d->rehashidx];
+        while(de) {
+            unsigned int h;
+            nextde = de->next;
+            h = dictHashKey(de->key) & d->ht[1].sizemask;
+            de->next = d->ht[1].table[h];
+            d->ht[1].table[h] = de;
+            d->ht[0].used--;
+            d->ht[1].used++;
+            de = nextde;
+        }
+        d->ht[0].table[d->rehashidx] = NULL;
+        d->rehashidx++;
+    }
+
+    if(d->ht[0].used == 0) {
+        free(d->ht[0].table);
+        d->ht[0] = d->ht[1];
+        _dictReset(&d->ht[1]);
+        d->rehashidx = -1;
+    }
+    return 1;
+}
+
+longlong timeInMilliseconds(void) {
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    return (((long long)tv.tv_sec)*1000) + (tv.tv_usec/1000);
+}
+
+int dictRehashMilliseconds(dict *d, int ms) {
+    long long start = timeInMilliseconds();
+    int rehashres = 0;
+
+    while(dictRehash(d, 100)) {
+        rehashres += 100;
+        if((timeInMilliseconds() - start) > ms) break;
+    }
+    return rehashres;
+}
--- source/dict.h
+++ source/dict.h
@@ -15,6 +15,7 @@
 #define DICT_OK		0
 #define DICT_ERR	1
 
+/* 字典条目 */
 typedef struct dictEntry
 {
 	void *key;
@@ -28,4 +29,102 @@ typedef struct dictEntry
 	struct dictEntry *next;
 }dictEntry;
 
-#endif
+/* 哈希表 */
+typedef struct dictht
+{
+    dictEntry **table; //哈希桶(Hash Bucket)的数组
+    unsigned long size; // 哈希表桶的个数
+    unsigned long sizemask; // 哈希表掩码，用于计算散列地址
+    unsigned long used; // 已经使用的哈希桶个数
+}dictht;
+
+/* 字典类型 */
+typedef struct dictType {
+    unsigned int (*hashFunction)(void *key); // 计算key散列值的哈希函数指针
+    void *(*keyDup)(void *privdata, const void *key); // 键复制的函数指针
+    void *(*valDup)(void *privdata, const void *obj); // 值复制的函数指针
+    void (*keyDestructor)(void *privdata, const void *key); // 释放键的函数指针
+    void (*valDestructor)(void *privdata, const void *val); // 释放值的函数指针
+    int (*keyCompare)(void *privdata, const void *key1, const void *key2); // 值比较函数指针
+}dictType;
+
+typedef struct dict {
+    dictType *type; // 字典类型
+    void *privdata; // ???
+    dictht ht[2]; // 双哈希表
+    long rehashidx; // rehashing索引
+    unsigned long iterators; // 该字典上目前运行的迭代器数目
+}dict;
+
+/* 字典的迭代器 */
+typedef struct dictIterator {
+    dict *d; // 指向字典的指针
+    long index; // ???
+    int table, safe;
+    dictEntry *next, *nextEntry; // ???
+    long long fingerprint; // 迭代器指纹
+}dictIterator;
+
+typedef void (dictScanFunction)(const void *privdata, dictEntry *de); // ???
+
+/* 哈希表的初始化大小 */
+#define DICT_HT_INITIAL_SIZE    4
+
+/*****************************宏定义***************************/
+/* 释放值 */
+#define dictFreeVal(d, entry) do{\
+}while(0)
+
+/* 设置字典条目值得宏函数 */
+#define dictSetVal(d, entry, _val_) do{\
+    if((d)->type->valDup) \
+        entry->v.val = (d)->type->valDup((d)->private, _val_); \
+    else \
+        entry->v.val = _val__; \
+}while(0)
+
+/* 设置有符号数 */
+#define dictSetSignedIntegerVal(d, entry, _val_) do{ \
+    entry->v.s64 = _val_; \
+}while(0)
+
+/* 设置无符号数 */
+#define dictSetUnsignedIntegerVal(d, entry, _val_) do{ \
+    entry->v.u64 = _val_; \
+}while(0)
+
+/* 设置双精度浮点数 */
+#define dictSetDoubleVal(d, entry, _val_) do{ \
+    entry->v.d = _val_; \
+}while(0)
+
+/* 设置key值 */
+#define dictSetKey(d, entry, _key_) do{ \
+    if((d)->type->keyDup) \
+        entry->v.val = (d)->keyDup((d)->privdata, _key_) \
+}while(0)
+
+/* 释放指定的键 */
+#define dictFreeKey(d, entry) do{\
+    if((d)->keyDestructor) \
+        (d)->keyDestructor(d->privdata, (entry)->key); \
+}while(0)
+
+/* 比较两个键值 */
+#define dictCompareKey(d, key1, key2) \
+    ((d)->type->keyCompare) ? \
+        (d)-type->keyCompare((d)->privdata, key1, key2) : \
+        ((key1) == (key2)) \
+
+#define dictHashKey(key) (d)->type->hashFunction(key) // 计算key的值
+#define dictGetKey(de) ((d)->key) // 获取entry的键
+#define dictGetVal(de) ((d)->v.val) // 获取entry的值
+#define dictIsRehashing(d) ((d)->rehashidx != -1) // 哈希表是否正在rehashing
+
+/*****************api函数*********************/
+dict *dictCreate(dictType *type, void *privdata); // 创建字典
+int dictResize(dict *d); // 重新设定哈希表的大小
+int dictExpand(dict *d, unsigned long size); // 扩展字典，哈希表rehashing
+int dictAdd(dict *d, void *key, void *val); // 字典中加入一个条目
+/*****************hash table类型**************/
+#endif /* __DICT_H_ */
